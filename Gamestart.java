370b7368181dd7239181fae877adf3ee22254128
softramesh29 <softramesh29@gmail.com>
2022-04-15 07:31:43 +0530

    Game start with 0 position

---
+++ Gamestart.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.SnakeandLadder;
+
+public class Gamestart {
+	public static void main(String[] args) {
+	int startPosition = 0;
+	int winnningPosition = 100;
+	String playerName = "Ramesh";
+	System.out.println( playerName + " in " + startPosition + " position " );
+	}
+}
